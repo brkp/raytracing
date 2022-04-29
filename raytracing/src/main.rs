31c7f558371044c544fa25d719b58c8e875508fa
brkp <brkp@tutanota.com>
2022-04-29 23:23:32 +0300

    move vec into the cgmath crate

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -1,9 +1,11 @@
-mod vec;
-
+use cgmath::vec::Vec3;
 use canvas::{color, Canvas};
 
 fn main() {
+    let vec = Vec3::new(1.0, 1.0, 1.0);
     let mut canvas = Canvas::new(500, 500);
+
+    println!("{:#?}", vec);
 
     for i in -canvas.hw..=canvas.hw {
         canvas.set_pixel(i, 0, color::WHITE);
