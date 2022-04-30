acd78ebca8d55c5e4f4e37303540072a54f42267
brkp <brkp@tutanota.com>
2022-04-30 17:51:51 +0300

    Hittable trait and HitRecord struct

---
+++ raytracing/src/hit.rs
@@ -0,0 +1,12 @@
+use cgmath::{Point3, Vec3};
+use crate::ray::Ray;
+
+pub struct HitRecord {
+    pub t: f32,
+    pub p: Point3,
+    pub n: Vec3,
+}
+
+pub trait Hittable {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
+}
