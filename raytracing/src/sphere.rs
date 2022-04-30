c4f25e513a443ad0ea326b4a03134e5f66185a8f
brkp <brkp@tutanota.com>
2022-04-30 22:35:35 +0300

    implement the `hit` function for the `Sphere` type

--- raytracing/src/sphere.rs
+++ raytracing/src/sphere.rs
@@ -5,13 +5,40 @@ use crate::ray::Ray;
 use crate::hit::{HitRecord, Hittable};
 
 pub struct Sphere {
-    rad: f32,
-    pos: Point3,
-    color: Color,
+    pub rad: f32,
+    pub pos: Point3,
+    pub color: Color,
+}
+
+impl Sphere {
+    pub fn new(pos: Point3, rad: f32, color: Color) -> Self {
+        Sphere { rad, pos, color }
+    }
 }
 
 impl Hittable for Sphere {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
-        todo!()
+        let oc = ray.origin - self.pos;
+
+        let a: f32 = ray.direction.len2();
+        let h: f32 = ray.direction.dot(oc);
+        let c: f32 = oc.len2() - self.rad.powf(2.0);
+        let d: f32 = h * h - a * c;
+
+        if d >= 0.0 {
+            let d = d.sqrt();
+            let r = (-h + d) / a;
+
+            if (t_min..=t_max).contains(&r) {
+                let p = ray.at(r);
+                return Some(HitRecord {
+                    t: r,
+                    p,
+                    n: (p - self.pos) / self.rad,
+                });
+            }
+        }
+
+        None
     }
 }
