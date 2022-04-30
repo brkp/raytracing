8f4475ab1db992251c4b33575f55dee8da5e4c3d
brkp <brkp@tutanota.com>
2022-04-30 23:49:41 +0300

    render a single Sphere

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -1,16 +1,36 @@
-use cgmath::vec::Vec3;
+#![allow(warnings)]
+
+mod camera;
+mod hit;
+mod ray;
+mod scene;
+mod sphere;
+
 use canvas::{color, Canvas};
+use cgmath::{Point3, Vec3};
+
+use crate::camera::Camera;
+use crate::hit::Hittable;
+use crate::scene::{Scene, Viewport};
 
 fn main() {
-    let vec = Vec3::new(1.0, 1.0, 1.0);
-    let mut canvas = Canvas::new(500, 500);
+    let aspect_ratio: f32 = 16.0 / 9.0;
+    let canvas_width: u32 = 400;
+    let canvas_height = (canvas_width as f32 / aspect_ratio) as u32;
 
-    println!("{:#?}", vec);
+    let mut canvas = Canvas::new(canvas_width, canvas_height);
+    let mut mscene = scene::Scene::new(
+        Camera::default(),
+        Viewport::new(2.0 * aspect_ratio, 2.0, -1.0),
+    );
 
-    for i in -canvas.hw..=canvas.hw {
-        canvas.set_pixel(i, 0, color::WHITE);
-        canvas.set_pixel(0, i, color::WHITE);
-    }
+    mscene.shapes.push(Box::new(sphere::Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        color::RED,
+    )));
 
-    canvas.save("main.bmp").unwrap();
+    canvas.clear(color::Color::rgb(0x6495ed));
+    mscene.render(&mut canvas);
+    canvas.export("main.bmp").unwrap();
 }
